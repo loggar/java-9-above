e52d10a0d4c0d98c35bc189ecfe7300f2945c6aa
Charly LEE <charly.loggar@gmail.com>
2021-11-11 17:02:34 +1100

    feat (string) template literals

--- src/main/java/com/loggar/statements/TemplateLiterals.java
+++ src/main/java/com/loggar/statements/TemplateLiterals.java
@@ -3,10 +3,10 @@ package com.loggar.statements;
 public class TemplateLiterals {
 	public static void main(String args[]) {
 		String json = """
-				  {
-				    "animal" : "Quokka",
-				    "link" : "https://en.wikipedia.org/wiki/Quokka"
-				  }
+				{
+				  "animal" : "Quokka",
+				  "link" : "https://en.wikipedia.org/wiki/Quokka"
+				}
 				""";
 
 		System.out.println(json);
